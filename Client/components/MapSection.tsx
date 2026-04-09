7df78d2dd2126bc6ccf7b88d1de73f4d26d43a74
sureshchavn <suresh2510.chavan@gmail.com>
2026-04-09 21:49:01 +0530

    changes to MapSection.tsx

--- Client/components/MapSection.tsx
+++ Client/components/MapSection.tsx
@@ -6,8 +6,7 @@ import { useEffect } from "react";
 // Fix marker icons (Leaflet bug in React)
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon-2x.png",
+  iconRetinaUrl:"https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon-2x.png",
   iconUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png",
   shadowUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png",
 });
